6db03a56dc1fb910dd6a1832a4c1a0b761f4ebf1
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-03-03 15:56:42 +0300

    Add main runner class

---
+++ src/main/java/com/epam/laboratory/RunetCollectionOfQuotes.java
@@ -0,0 +1,14 @@
+package com.epam.laboratory;
+
+import java.util.Scanner;
+
+public class RunetCollectionOfQuotes {
+    public static void main( String[] args ) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Print number of quote: ");
+        String number = scanner.nextLine();
+        System.out.println(new WebPageLoader().getQuoteByNumber(number));
+    }
+}
