c82b9bfe68706050151a2a84f1f38652addc4787
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-03-03 15:54:49 +0300

    Add exceptions

---
+++ src/main/java/com/epam/laboratory/exceptions/NotANumberException.java
@@ -0,0 +1,8 @@
+package com.epam.laboratory.exceptions;
+
+public class NotANumberException extends Exception {
+
+    public NotANumberException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/epam/laboratory/exceptions/NumberOutOfRangeException.java
@@ -0,0 +1,8 @@
+package com.epam.laboratory.exceptions;
+
+public class NumberOutOfRangeException extends Exception {
+
+    public NumberOutOfRangeException(String message) {
+        super(message);
+    }
+}
