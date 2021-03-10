aa04a4a7c4bf162ef9fbaa342335ce289b2dcca3
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-03-10 10:31:27 +0300

    Remove NumberOutOfRangeException.java

--- src/main/java/com/epam/laboratory/exceptions/NumberOutOfRangeException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.laboratory.exceptions;
-
-public class NumberOutOfRangeException extends Exception {
-
-    public NumberOutOfRangeException(String message) {
-        super(message);
-    }
-}
